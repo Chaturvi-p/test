e65dc197e4363e73b44c7be97a26531fa730e2a6
Chaturvi_P <chaturvipbtech24@gmail.com>
2026-02-19 12:42:21 +0530

    update x.py

--- x.py
+++ x.py
@@ -1,4 +1,13 @@
-a=2
-b=3
-c=a+b
-print(c)
+"""Simple addition script."""
+
+
+def main():
+    """Compute and print sum of two numbers."""
+    a = 2
+    b = 3
+    c = a + b
+    print(c)
+
+
+if __name__ == "__main__":
+    main()
